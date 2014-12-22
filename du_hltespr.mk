c02022fd6c9d52a004bc4687e0b08fb1a568b354
moepda <moepda@gmail.com>
2014-12-22 00:16:49 +0000

    lets get dirty...unicorns
    Conflicts:
    	cm.mk

--- du_hltespr.mk
+++ du_hltespr.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/hltespr/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_hltespr
+PRODUCT_NAME := du_hltespr
 PRODUCT_DEVICE := hltespr
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
