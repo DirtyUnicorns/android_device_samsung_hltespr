3d207b623017c86f81c62c567c83e1c61ae47896
moepda <moepda@gmail.com>
2015-03-21 21:43:36 +0000

    hltespr : switch to unicornblood defconfig
    Change-Id: I1514488c9f8fd30028324e75c72da3dd725f48a8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ TARGET_OTA_ASSERT_DEVICE := SM-N900P,hltespr
 TARGET_BOOTLOADER_BOARD_NAME := hltespr
 
 # Kernel
-TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_hlte_spr_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := unicornblood_hlte_spr_defconfig
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
